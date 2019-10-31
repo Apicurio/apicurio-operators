66225a6c45e373ffd88119c15f9e5844980fb135
Luis Garcia Acosta <lgarciaac@gmail.com>
2019-10-31 17:55:38 +0100

    Reintroduce Image to the CR (#15)
    * Reintroduce Image to the CR
    
    With this PR `Spec.Image` is reintroduced as a parameter to Apicurito. If specified, `Spec.Image` will replace the default value from configuration file.
    
    Other changes:
     - Reformat files
     - Change *apicuritosv1alpha* for *v1alpha* to simplify name conventions
    
    * Typo
    
    * Delete command from deploy
    
    It is built into the Docker container

--- apicurito/pkg/properties/properties.go
+++ apicurito/pkg/properties/properties.go
@@ -17,47 +17,89 @@
 package properties
 
 import (
-    "encoding/json"
-    "github.com/apicurio/apicurio-operators/apicurito/pkg/configuration"
-    "io/ioutil"
-    "k8s.io/apimachinery/pkg/util/yaml"
-    "strings"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+
+	"github.com/imdario/mergo"
+
+	"github.com/apicurio/apicurio-operators/apicurito/pkg/apis/apicur/v1alpha1"
+	"github.com/apicurio/apicurio-operators/apicurito/pkg/configuration"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 type Properties struct {
-    Image string
+	Image string
 }
 
 // Returns all processed properties for Apicurito
-func GetProperties() (*Properties, error) {
-    c := configuration.GetConfiguration()
-    p, err := loadFromFile(c.ConfigFile)
-    if err != nil {
-        return nil, err
-    }
+func GetProperties(apicurito *v1alpha1.Apicurito) (*Properties, error) {
+	c := configuration.GetConfiguration()
+	cp, err := loadFromFile(c.ConfigFile)
+	if err != nil {
+		return nil, err
+	}
 
-    return p, nil
+	ap, err := loadFromApicurito(apicurito)
+	if err != nil {
+		return nil, err
+	}
+
+	rp, err := mergeProperties(*cp, *ap)
+	if err != nil {
+		return nil, err
+	}
+
+	return rp, nil
 }
 
 // Load configuration from config file. Config file is expected to be a yaml
 // The returned configuration is parsed to JSON and returned as Properties
-func loadFromFile(config string ) (*Properties, error) {
-    data, err := ioutil.ReadFile(config)
-    if err != nil {
-        return nil, err
-    }
+func loadFromFile(config string) (*Properties, error) {
+	data, err := ioutil.ReadFile(config)
+	if err != nil {
+		return nil, err
+	}
 
-    if strings.HasSuffix(config, ".yaml") || strings.HasSuffix(config, ".yml") {
-        data, err = yaml.ToJSON(data)
-        if err != nil {
-            return nil, err
-        }
-    }
+	if strings.HasSuffix(config, ".yaml") || strings.HasSuffix(config, ".yml") {
+		data, err = yaml.ToJSON(data)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-    p := &Properties{}
-    if err := json.Unmarshal(data, p); err != nil {
-        return nil, err
-    }
+	p := &Properties{}
+	if err := json.Unmarshal(data, p); err != nil {
+		return nil, err
+	}
 
-    return p, nil
+	return p, nil
+}
+
+// From apicurito CR, Unmarshal it into a property object
+func loadFromApicurito(apicurito *v1alpha1.Apicurito) (*Properties, error) {
+	p := &Properties{}
+
+	jsonProperties, err := json.Marshal(apicurito.Spec)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(jsonProperties, p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// Merge two Property objects and overwrite config values with apicurito values
+// if the fields in apicurito aren't set to its 0 value.
+func mergeProperties(config Properties, apicurito Properties) (*Properties, error) {
+	err := mergo.Merge(&config, apicurito, mergo.WithOverride)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
