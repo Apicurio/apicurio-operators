42583a950f31b74b0bdc9bdbc28ed4297b28c41b
Fabian Martinez <famartin@redhat.com>
2020-05-12 10:15:42 +0200

    remove unneeded-steps

--- .github/scripts/install_dep.sh
+++ .github/scripts/install_dep.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Update archives"
-
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt-get update
-
-echo "Install golang"
-sudo apt-get install golang-1.13
+echo "Set up golang"
 export GO111MODULE='on'
 
 echo "Install operator sdk"
@@ -17,18 +11,3 @@ sudo mv operator-sdk /usr/local/bin/operator-sdk
 
 echo "Clean cache"
 sudo apt-get clean
-
-#Docker installation from google default repositories
-sudo apt-get -y update
-
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-sudo mkdir -p /mnt/docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get -y update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
