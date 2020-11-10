a98b0b9f8cf44ca126bbf88d18aeb2039eaa142a
Luis Garcia Acosta <lgarciaac@gmail.com>
2020-11-10 13:01:14 +0100

    remove image from api

--- apicurito/scripts/go-build.sh
+++ apicurito/scripts/go-build.sh
@@ -10,8 +10,6 @@ go generate ./...
 if [[ -z ${CI} ]]; then
     ./scripts/go-test.sh
     operator-sdk build ${REGISTRY}/${IMAGE}:${TAG}
-   
 else
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -o build/_output/bin/apicurito -mod=vendor github.com/apicurio/apicurio-operators/apicurito/cmd/manager
-
 fi
