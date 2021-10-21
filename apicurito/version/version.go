033d7f4703f72a85f6ca7051776c962c90b61d1f
Claudio Miranda <claudio@claudius.com.br>
2021-10-21 17:18:19 -0300

    ENTESB-17647 Apicurito pods contain metering labels with incorrect values https://issues.redhat.com/browse/ENTESB-17647

--- apicurito/version/version.go
+++ apicurito/version/version.go
@@ -3,6 +3,7 @@ package version
 import "strings"
 
 var (
+	// these version are set at build time, see scripts/go-build.sh, the version values come from config/vars/Makefile
 	Version      = "7.9.0"
 	PriorVersion = "0.2.0"
 )
