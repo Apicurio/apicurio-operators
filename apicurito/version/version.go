228b00d8bb63dfcdc3ebf1986e217dbe377ddb03
Claudio Miranda <claudio@claudius.com.br>
2021-11-08 12:05:49 -0300

    Update version to 7.10

--- apicurito/version/version.go
+++ apicurito/version/version.go
@@ -4,8 +4,8 @@ import "strings"
 
 var (
 	// these version are set at build time, see scripts/go-build.sh, the version values come from config/vars/Makefile
-	Version      = "7.9.0"
-	PriorVersion = "0.2.0"
+	Version      = "7.10.0"
+	PriorVersion = "7.9.0"
 )
 
 // Return the major.minor, as 7.8, instead of 7.8.0
