74c4c386e4854f6787ff27109cf6ba92d7ae5336
Paul Richardson <parichar@redhat.com>
2022-02-28 10:46:15 +0000

    Updates for 7.10.1
    * Updates the version.go file
    
    * Ensures local build script updates version.go but also fails
      if the build failed
    
    * Makefile bumps to 7.10.1

--- apicurito/scripts/go-build.sh
+++ apicurito/scripts/go-build.sh
@@ -10,6 +10,10 @@ GOFLAGS="-X github.com/apicurio/apicurio-operators/apicurito/pkg/cmd.GitCommit=$
 GOFLAGS=$GOFLAGS" -X github.com/apicurio/apicurio-operators/apicurito/version.Version=${FUSE_VERSION}"
 GOFLAGS=$GOFLAGS" -X github.com/apicurio/apicurio-operators/apicurito/version.PriorVersion=${FUSE_PREV_VERSION}"
 
+# Updates version/version.go
+sed -i "s/Version      = .*/Version      = \"${FUSE_VERSION}\""/ version/version.go
+sed -i "s/PriorVersion = .*/PriorVersion = \"${FUSE_PREV_VERSION}\""/ version/version.go
+
 go mod vendor
 go generate ./...
 
@@ -19,5 +23,9 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
   go build -ldflags "${GOFLAGS}" \
   -o build/_output/bin/apicurito \
   -mod=vendor github.com/apicurio/apicurio-operators/apicurito/cmd/manager
+if [ $? != 0 ]; then
+  echo "Error: build failed"
+  exit 1
+fi
 
 docker build . -t $REGISTRY/$IMAGE:$TAG
--- apicurito/version/version.go
+++ apicurito/version/version.go
@@ -2,6 +2,6 @@ package version
 
 var (
 	// these version are set at build time, see scripts/go-build.sh, the version values come from config/vars/Makefile
-	Version      = "7.10.0"
-	PriorVersion = "7.9.0"
+	Version      = "7.10.1"
+	PriorVersion = "7.10.0"
 )
